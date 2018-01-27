67709c4621602378634c896e309a6a91966e8f00
kakehi <takuma.kakehi@gmail.com>
2018-01-26 21:39:35 -0500

    previous version before moving forwrd

--- public/mail.php
+++
@@ -1,9 +0,0 @@
-<?php 
- echo "<script type='text/javascript'>alert('AAAA');</script>";
-  $email = $_POST['email'];
-  $recipient = 'takuma.kakehi@gmail.com';
-  $formcontent= $_POST['message'];     
-  $subject = "PORTFOLIO : contact form";
-  $mailheader = "From: $email \r\n";
-  mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-?>
